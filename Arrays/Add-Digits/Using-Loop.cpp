fe8ef460e6756103d17c254768ebd473f905a775
Shivam Soni <shivamkmv2311@gmail.com>
2022-02-08 08:39:23 +0530

    Create Using-Loop.cpp

---
+++ Arrays/Add-Digits/Using-Loop.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int res=0;
+        
+        while(num>9)
+        {
+            int temp=num%10;
+            num=num/10;
+            num+=temp;
+        }
+        
+        return num;
+        
+    }
+};
