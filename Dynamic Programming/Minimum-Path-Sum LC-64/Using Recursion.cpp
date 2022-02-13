44697089b6e227f31dbd004ecad89e5aa2819483
Shivam Soni <shivamkmv2311@gmail.com>
2022-02-13 16:44:30 +0530

    Create Using Recursion.cpp

---
+++ Dynamic Programming/Minimum-Path-Sum LC-64/Using Recursion.cpp
@@ -0,0 +1,22 @@
+//TC: Exponential
+
+class Solution {
+public:
+    
+    int helper(vector<vector<int>>& grid, int row, int col)
+               {
+                   if(row<0 || col<0)
+                       return INT_MAX;
+                   if(row==0 and col==0)
+                       return grid[row][col];
+                   
+                   return grid[row][col] + min(helper(grid,row-1,col),helper(grid,row,col-1));
+               }
+    int minPathSum(vector<vector<int>>& grid) {
+        
+        return helper(grid,grid.size()-1,grid[0].size()-1);
+        
+        
+        
+    }
+};
