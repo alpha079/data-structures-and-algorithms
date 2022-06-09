fb52a0cb6b0ed77adf2aea0d77e498b20ae75d24
Shivam Soni <shivamkmv2311@gmail.com>
2022-06-09 19:54:27 +0530

    Update Using-Sliding-Window.cpp

--- Dynamic Sliding Window/max-consecutive-ones-iii/Using-Sliding-Window.cpp
+++ Dynamic Sliding Window/max-consecutive-ones-iii/Using-Sliding-Window.cpp
@@ -24,3 +24,26 @@ public:
         
     }
 };
+
+//Other Sliding Window Solution 
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        
+        int left=0;
+        int zero=0;
+        int res=0;
+        for(int right=0;right<nums.size();right++)
+        {
+            if(nums[right]==0)
+                zero++;
+            if(zero>k)
+            {
+                if(nums[left]==0)zero--;
+                left++;
+            }
+            res=max(res,right-left+1);
+        }
+        return res;
+    }
+};
