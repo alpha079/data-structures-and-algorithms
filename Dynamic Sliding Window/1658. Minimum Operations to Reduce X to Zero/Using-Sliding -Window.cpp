a8570a8f3ef7fe75425431baf804051feaa55cc4
Shivam Soni <shivamkmv2311@gmail.com>
2022-06-12 15:14:45 +0530

    Update Using-Sliding -Window.cpp

--- Dynamic Sliding Window/1658. Minimum Operations to Reduce X to Zero/Using-Sliding -Window.cpp
+++ Dynamic Sliding Window/1658. Minimum Operations to Reduce X to Zero/Using-Sliding -Window.cpp
@@ -1,3 +1,22 @@
+/*
+//Post Link: https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/discuss/1080880/Are-you-stuck-once-read-this-.-logic-explained-clearly-or-easy-to-understand
+so , the problem is saying that we can remove either leftmost or rightmost element and we have to remove them in such a way that
+1.remove minimum number of elements and
+2. sum of all removed element is X
+
+Now main point is that if successfully remove elements such that sum of removed elements is X , then
+
+sum of remaining array elements will be (sum-X)
+remaing array will be subarray of original array.
+
+So , now our task is find subaarray whose sum is (sum - X) and its length is maximum ( because we want to remove minimum number of elements , so we will have to maximise remianing element ) .
+
+time - O(N)
+space - O(1)
+
+*/
+
+
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
